966dbc8df86294386189d61d130c6cd0ef007534
alvarogamez28 <alvarogamjim2ciclo@gmail.com>
2021-11-22 18:53:18 +0100

    ejer

--- Boletin UT4/ejercicio20.js
+++ Boletin UT4/ejercicio20.js
@@ -1,17 +1,11 @@
-var array = new Array ();
-var arrayBidimensional = new Array();
-
-do {
-    var datos = prompt("Introduce nombre, apellidos, DNI y fecha de nacimiento");
-    if (!datos==""){
-        array=datos.split(",");
-        arrayBidimensional.push(array);
-    }
-    
-    
-} while (datos!="");
-
-
-document.write(arrayBidimensional);
-
+var array = [];
+    var arrayBidimensional = [];
+    do{
+        var datos = prompt("Introduce nombre, apellidos, DNI y fecha de nacimiento separado por comas");
+        if(!datos == ""){
+            array = datos.split(",");
+            arrayBidimensional.push(array);
+        }
+    }while(datos != "");
+    console.log(arrayBidimensional);
 
